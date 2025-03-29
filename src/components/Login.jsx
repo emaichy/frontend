390d5c057eb5983f93baf1f864c560cad7f79997
emaichy <lalomonto16@gmail.com>
2025-03-29 13:09:47 -0600

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const response = await fetch("http://127.0.0.1:5000/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Error en el inicio de sesión");
+      }
+
+      localStorage.setItem("token", data.token);
+      alert("Login exitoso");
+      navigate("/users"); // Redirige a la página de usuarios
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Iniciar Sesiónes</h2>
+      {error && <p className="error-message">{error}</p>}
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Correo"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Ingresar</button>
+      </form>
+      <button onClick={() => navigate("/register")} className="register-button">
+        Crear Usuario
+      </button>
+    </div>
+  );
+};
+
+export default Login;
+
